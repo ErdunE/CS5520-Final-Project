c31fd25bae3a9cc4432e9bbd2e19a99d35cf6eee
Erdun E <eed950128@gmail.com>
2025-03-14 23:24:01 -0400

    Implement habit list RecyclerView, completion CheckBox, UI updates, and habit dialog placeholder

--- app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
+++ app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
@@ -2,23 +2,45 @@ package edu.northeastern.finalproject_group_1;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private HabitAdapter habitAdapter;
+    private List<Habit> habitList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Testing
+        habitList = new ArrayList<>();
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
+        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
+
+        habitAdapter = new HabitAdapter(habitList);
+        recyclerView.setAdapter(habitAdapter);
     }
 }
