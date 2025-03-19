9cfb4b8506e326f70aebf20feb7df2362f850c88
Jenny Nguyen <>
2025-03-19 00:00:16 -0700

    Implemented Nav Menu and bottom navigation view. Added activity's to show that clicking the icon bottoms will open a new activity.

--- app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
+++ app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
@@ -1,21 +1,53 @@
 package edu.northeastern.finalproject_group_1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class DashboardActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.Shop) {
+                    startActivity(new Intent(DashboardActivity.this, MarketplaceActivity.class));
+                    return true;
+                } else if (itemId == R.id.Challenges) {
+                    startActivity(new Intent(DashboardActivity.this, ChallengesActivity.class));
+                    return true;
+                } else if (itemId == R.id.Stats) {
+                    startActivity(new Intent(DashboardActivity.this, StatsActivity.class));
+                    return true;
+                } else if (itemId == R.id.Settings) {
+                    startActivity(new Intent(DashboardActivity.this, SettingsActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.statsTV), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
