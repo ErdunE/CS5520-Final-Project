31c98c35ef5de712c8d4445816c88b5f3681878c
Erdun E <eed950128@gmail.com>
2025-03-20 19:53:45 -0400

    Move habit list to Fragment & integrate Garden view

--- app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
+++ app/src/main/java/edu/northeastern/finalproject_group_1/DashboardActivity.java
@@ -1,46 +1,23 @@
 package edu.northeastern.finalproject_group_1;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private HabitAdapter habitAdapter;
-    private List<Habit> habitList;
+    private ViewPager2 viewPager;
+    private DashboardPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        viewPager = findViewById(R.id.viewPager);
+        pagerAdapter = new DashboardPagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
 
-        // Testing
-        habitList = new ArrayList<>();
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-        habitList.add(new Habit("Drink Water", "Drink 8 glasses of water", false, R.drawable.baseline_water_drop_24, "Daily", 10));
-        habitList.add(new Habit("Exercise", "30 minutes workout", false, R.drawable.baseline_fitness_center_24, "Daily", 20));
-
-        habitAdapter = new HabitAdapter(habitList);
-        recyclerView.setAdapter(habitAdapter);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
     }
 }
---
+++ app/src/main/java/edu/northeastern/finalproject_group_1/DashboardPagerAdapter.java
@@ -0,0 +1,31 @@
+package edu.northeastern.finalproject_group_1;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class DashboardPagerAdapter extends FragmentStateAdapter {
+
+    public DashboardPagerAdapter(@NonNull FragmentActivity activity) {
+        super(activity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new GardenFragment();
+            case 1:
+                return new HabitListFragment();
+            default:
+                return new GardenFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
