0a8ec5cb3d58059c2bbca144d00d5add8d7fa349
Erdun E <eed950128@gmail.com>
2025-03-25 22:43:55 -0400

    Preset Icons

---
+++ app/src/main/java/edu/northeastern/finalproject_group_1/IconData.java
@@ -0,0 +1,45 @@
+package edu.northeastern.finalproject_group_1;
+
+import edu.northeastern.finalproject_group_1.R;
+
+public class IconData {
+
+    public static final int[] ICON_RES_IDS = {
+            R.drawable.ic_check,
+            R.drawable.ic_calendar,
+            R.drawable.ic_alarm,
+            R.drawable.ic_bedtime,
+            R.drawable.ic_restaurant,
+            R.drawable.ic_drink_water,
+            R.drawable.ic_running,
+            R.drawable.ic_book,
+            R.drawable.ic_edit,
+            R.drawable.ic_code,
+            R.drawable.ic_school,
+            R.drawable.ic_translate,
+            R.drawable.ic_psychology,
+            R.drawable.ic_fitness,
+            R.drawable.ic_spa,
+            R.drawable.ic_self_improvement,
+            R.drawable.ic_favorite,
+            R.drawable.ic_hospital,
+            R.drawable.ic_brush,
+            R.drawable.ic_music,
+            R.drawable.ic_mic,
+            R.drawable.ic_camera,
+            R.drawable.ic_palette,
+            R.drawable.ic_game,
+            R.drawable.ic_movie,
+            R.drawable.ic_social,
+            R.drawable.ic_chat,
+            R.drawable.ic_travel,
+            R.drawable.ic_park,
+            R.drawable.ic_eco
+    };
+
+    public static int getDefaultIcon() {
+        return R.drawable.ic_favorite;
+    }
+
+
+}
